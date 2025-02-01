0a24930030b304a4d17e43d1889bdf517f765b92
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-01 22:16:05 +0530

    explore array

---
+++ Test.java
@@ -0,0 +1,77 @@
+/**
+what is array 
+array are fixed in size
+array can hold homongeous data element
+1.array declaration 
+2.array creation  
+*/
+
+public class Test{
+	public static void main(String args[]){
+		/*
+		*Array are fixed size 
+		*/
+		//System.out.println(args.length);
+		/*
+		* Every arrayin java is object. 
+		*/
+//error: integer number too large
+//int[] x = new int[2147483648];
+		//int[] x = new int[2147483647];
+		
+//arrayInttalization
+// we create array evary element by default.
+		//int[] x= new int[3];
+//        system.out.println(x);
+//        system.out.println(x[0]);
+		
+// Array declaration
+// initialize an array into singleline.
+        int [] x;
+// error: illegal start of expression
+                
+// x={10,20,30};
+		 
+//Length vs length
+//  int []a = new int[10];
+	
+//Array element assigment 
+//  int [] a = new int [10]; 
+//rror: long int 
+//[4] = 10l;
+//Array variable assigment.
+    int [] x = {10,20,30,40};
+//error: not find symbol
+        int[] c =ch;
+		
+//type of variable
+//localvariable:we have to create variable inside method or block or construction.
+//error:cannot find symbol.
+//	int i = 0;
+//		for(intj=0;j<3;j++){
+//			i=i+j;
+//		}
+//		System.out.println(i+" "+j);
+//
+
+
+   
+   
+   
+   
+                 
+
+ 
+		    
+		
+		
+		
+		
+	}
+	
+}
+
+
+
+
+
