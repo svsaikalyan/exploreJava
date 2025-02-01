284a14a3700fc01e1f1897e5a794e96deba7baf5
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-01 22:28:11 +0530

    explore Operators

---
+++ Operators.java
@@ -0,0 +1,31 @@
+/**
+*what is increment and decrement.
+increment : pre-increment and post-increment
+decrement : pre-decrement and post-decrement
+*/
+
+
+public class Operators{
+	public static void main (String[] args){
+//		int x = 10;
+//x error: unexpected type
+//		int y = ++10;
+	    System.out.println(y);
+	}
+	
+}
+
+/**
+public class Opertors
+{
+	public static void main(String[] args) {
+error:possible lossy conversion from int to byte
+	    byte a =10;
+	    byte b =20;
+	    byte c =a+b;
+		System.out.println(c);
+	}
+}
+*/
+
+ 
