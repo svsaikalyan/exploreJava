84453611eb42b7e1daf5b32fed60648be7790dfe
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-01 22:21:26 +0530

    explore CommandLine

---
+++ CommandLine.java
@@ -0,0 +1,14 @@
+/**
+*what is command line argument
+The argument which are passing command line argument are called command line arugment.
+
+*/
+
+
+public class CommandLine{
+	public static void main(String[] args){
+		for(int i=0;i<=args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
+}
