e829aa9e63b110a9cdb99698285c7ed8dfa5c6a8
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-02 20:25:46 +0530

    explore Bitwise

---
+++ Bitwise.java
@@ -0,0 +1,20 @@
+/**
+*what is bitwise Operator.
+&:And -return true iff both are true.
+|:Or - return true iff atleast one argument is true.
+^:x-or - if both operands are differnt then result is true.
+*/
+
+public class Bitwise{
+	public static void main (String[] args){
+//		System.out.println(true & false);
+//		System.out.println(true | false);
+//		System.out.println(true ^ false);
+//		System.out.println(4 & 5);
+//		System.out.println(4 | 5);
+//		System.out.println(4 ^ 5);
+ error: bad operand type boolean
+		System.out.println(~true);
+//		System.out.println(~4);
+	}
+}
