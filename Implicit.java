a71c348dd18b227ed39a552fa232c7cabd7c6872
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-03 16:22:16 +0530

    explore Implicit type casting

---
+++ Implicit.java
@@ -0,0 +1,17 @@
+/**
+*what is Implicit typecasting.
+compiler is the responsible to perform this typecasting.
+*what is explicit.
+programmer is responsible to perform this typecasting.
+*/
+
+
+public class Implicit{
+	public static void main(String[]args){
+		int x =150;
+		short s = (short)x;
+		System.out.println(s);
+//		double d =10;
+//		System.out.println(d);
+	}
+}
