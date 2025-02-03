f50208bdc4e97339804853e5ef553c16e20ee9ba
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-03 16:27:41 +0530

    explore Assigment simple chained compound assigment operator

---
+++ Assigment.java
@@ -0,0 +1,16 @@
+/**
+*what is assigment operators.
+simple operators
+chained assigment
+compound assigment
+*/
+
+public class Assigment{
+	public static void main(String[] args){
+//		int x = 10;
+//		int a,b,c,d;
+//		a=b=c=d=20;
+        
+
+	}
+}
