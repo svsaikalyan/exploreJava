4a34650cd9778ef75ccaaf2fc2c323b145dd3559
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-04 21:18:15 +0530

    explore flowControl

---
+++ FlowControl.java
@@ -0,0 +1,16 @@
+/**
+*what is flow control.
+flow control describes the order in which statement wii be executed at runtime.
+*/
+
+public class FlowControl{
+	public static void main(String[] args){
+		int x = 12;
+		if(x == 20){
+			System.out.println("hi");
+		}
+		else{
+			System.out.println("hello");
+		}
+	}
+}
