357f34a1154b5400c373ff55c71d969605e5fb63
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-05 15:53:03 +0530

    explore Dowhile

---
+++ LoopProgram.java
@@ -0,0 +1,15 @@
+/**
+*what is dowhile
+*/
+
+
+
+public class LoopProgram{
+	public static void main(String[] args){
+		int i = 1;
+		do{
+			System.out.println("i= "+i);
+			i++;
+		}while(i>4);
+	}
+}
