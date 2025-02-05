536524b2d89665d66c226c8e7cbf7e8e7ee92726
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-05 15:50:57 +0530

    explore WhileLoop

---
+++ While.java
@@ -0,0 +1,17 @@
+/**
+* what is while loop.
+If booleanVal evaluates to false in first loop/evaluation, then while loop statements will not be executed even once.
+*/
+
+
+
+public class While{
+	public static void main(String[] args){
+		int i = 0;
+		while(i > 4){
+			System.out.println("i="+i);
+			i++;
+			
+		}
+	}
+}
