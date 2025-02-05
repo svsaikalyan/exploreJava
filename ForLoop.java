2164e5737820c612344d7c753f831f3ebebad501
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-05 18:17:22 +0530

    explore forloop

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+/**
+*what is forLoop
+for loop is the most commonly loop in java .
+if we no number of iteration in advance then for loop is the best choice.
+*/
+
+public class ForLoop{
+	public static void main(String[] args){
+		for(int i=0;i<5;i++){
+			System.out.println("i="-i);
+		}
+	}
+}
