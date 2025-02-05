f70802830e9c23d7e4644a060db4efbf6fbb0823
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-05 20:15:37 +0530

    dig to SwitchProgarm

---
+++ SwitchProgram.java
@@ -0,0 +1,31 @@
+public class SwitchProgram{
+	public static void main(String[] args){
+		int day = 3;
+		System.out.println("enter the day");
+	
+		switch(day){
+			case 1:
+			System.out.println("sunday");
+			break;
+			case 2:
+			System.out.println("monday");
+			break;
+			case 3:
+			System.out.println("tuesday");
+			break;
+			case 4:
+			System.out.println("thursday");
+			break;
+			case 5:
+			System.out.println("firday");
+			break;
+			case 7:
+			System.out.println("saturday");
+			break;
+			default:
+			System.out.println("invalid days");
+			
+		}
+			
+	}
+}
