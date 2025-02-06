a13dbf06db606036959492ed97d3687c7c5a78cb
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-06 16:08:46 +0530

    explore Explicitclassimport

---
+++ ExplicitProgram.java
@@ -0,0 +1,19 @@
+/**
+*what is class import
+1.They are two type Explicitclassimport
+It is highly recommend to use Explicit class import because it improves readability of code. 
+2.Implicitclassimport
+It is highly recommend to use Explicit class import because it reduce readability of code.
+*/
+
+
+
+
+import java.util.*;
+import java.sql.*;
+public class ExplicitProgram{
+	public static void main (String[] args){
+		Date d = new Date();
+//error: reference to Date is ambiguous		
+	}
+}
