e503d80f095157693409132b2ea6bbdef46ed207
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-06 12:29:00 +0530

    explore AcessModifier

---
+++ AccessModifier.java
@@ -0,0 +1,25 @@
+/**
+*what is java source file structure.
+A java program can container any number of class but at most one class can be declare as public.
+*/
+
+
+class A {
+	public static void main (String[] args){
+		System.out.println(" A class main");
+	}
+}
+class B {
+	public static void main (String[] args){
+		System.out.println(" B class main");
+	}
+}
+class C {
+	public static void main (String[] args){
+		System.out.println(" C class main");
+	}
+}
+class D {
+//Error: Main method not found in class D, please define the main method as:
+		
+}
