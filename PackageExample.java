7c9c9d2c74f61524c9976cdab44dcf64cf455ed2
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-07 11:48:47 +0530

    explore PackageExample

---
+++ PackageExample.java
@@ -0,0 +1,13 @@
+/**
+*what is package.
+it is an excapsulation mechanism to group relation class and interface into a single unit nothing but package.
+*/
+package pack1;
+package pack2;
+
+public class PackageExample{
+	public static void main(String[] args){
+//  error: class, interface, enum, or record expected
+		System.out.println("package demo");
+	}
+}
