a265f4aff80325444d122b5dec09d7e06ff669a5
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-10 16:03:29 +0530

    explore create table

---
+++ sqlexample.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+Insert Into Persons values("01", "kiran", "sai", "hello nagar first line", "nepal");
+Insert Into Persons values("02", "nakul", "naman", "hi nagar second line ", "china");
+Insert Into Persons values("03", "naresh", "nari", " sivaji nagar line ", "namapally");
+Insert Into Persons values("04", "giri", "sai", "sailu nagar  ", "hyderbad");
+Insert Into Persons values("05", "mahesh", "sai", "srini nagar  line ", "snreddy");
+desc Persons
