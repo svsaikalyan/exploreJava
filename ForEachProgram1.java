756fd52f055f66580ea05cb0636b83e86cadeefd
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-11 19:44:20 +0530

    exploreEachexanple

---
+++ ForEachProgram1.java
@@ -0,0 +1,14 @@
+public class ForEachProgram{
+	public static void main(String[] args){
+		ArrayList<Integer> l = new ArrayList();
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		for(int m : l){
+			System.out.println(m);
+		
+		
+			
+			
+	}
+}
