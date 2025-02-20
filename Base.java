19babca12f45980beacfd876ad2e60742136fcbe
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-20 22:27:09 +0530

    exploreStaticExample

---
+++ Base.java
@@ -0,0 +1,20 @@
+class Base{
+	static int i = 10;
+	static{
+		m1();
+		System.out.println("first static block");
+		
+	}
+	public static void main(String[] args){
+		m1();
+		System.out.println("main method");
+	}
+	public static void m1(){
+		System.out.println(j);
+	}
+	static{
+		System.out.println("second static block");
+	}
+	static int j = 20;
+	
+}
