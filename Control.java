66b3ff7330e9b8880631f274598f4242cc19c603
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-21 17:29:33 +0530

    exploreinstancestaticexample

---
+++ Control.java
@@ -0,0 +1,20 @@
+class Control{
+	private static String m1(String msg)
+	{
+		System.out.println(msg);
+		return msg;
+	}
+	static String m = m1("1");
+	{
+		m = m1("2");
+	}
+	static
+	{
+		m =m1("3");
+	}
+	public static void main (String[] args)
+	{
+		Object obj = new Control();
+	}
+	
+}
