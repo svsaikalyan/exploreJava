2db22e15219cb5350004cf08447cc38c8a0b81df
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-23 18:09:58 +0530

    exploreHandlingExample

---
+++ Handling.java
@@ -0,0 +1,27 @@
+/*
+*what is expection.
+An unexpected unwanted event that disturb normal follow is called exception.
+/*
+
+
+
+
+
+
+class Handling 
+{
+	public static void main(String[] args)
+	{
+		doStuff();
+	}
+	public static void doStuff()
+	{
+		doMoreStuff();
+		//System.out.println(10/0);
+	}
+	public static void doMoreStuff()
+	{
+		System.out.println("Hello");
+		//System.out.println(10/0);
+	}
+}
