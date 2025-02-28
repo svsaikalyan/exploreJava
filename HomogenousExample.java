fd46a821565dbd8f254c832356fdd4dc07feb8be
saikalyansennam <saikalyansennam66@gmail.com>
2025-02-28 19:20:25 +0530

    exploreHomogenous

---
+++ HomogenousExample.java
@@ -0,0 +1,28 @@
+/*
+*what is an Homogenous
+Array ca hold homogenous datatype element.
+
+*/
+
+
+
+
+
+public class HomogenousExample{
+	public static void main(String[] args){
+		int[] number = new int[5];
+		//System.out.println(number.length);
+		number[0]=1;
+		number[1]=2;
+		number[2]=3;
+		number[3]=4;
+		number[4]=5;
+//		number[5]=6;
+//		number[6]=7;
+//		number[7]=8;
+		//System.out.println(number.length);
+		Object[] oArray = new Object[5];
+		oArray[0]=true;
+		oArray[1]=2;
+	}
+}
