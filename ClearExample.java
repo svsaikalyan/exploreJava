ee257912a5c1eda1898aad485622cae792c68d7f
saikalyansennam <saikalyansennam66@gmail.com>
2025-03-04 19:23:28 +0530

    ExploreClearExample

---
+++ ClearExample.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ClearExample{
+	public static void main(String[] args){
+		ArrayList<String> n = new ArrayList<>();
+		n.add("kiran");
+		n.add("sai");
+		n.add("kalyan");
+		n.add("kumar");
+		System.out.println(" "+n);
+		n.clear();
+		System.out.println(" "+n);
+		
+	}
+}
+
+
+//output: [kiran, sai, kalyan, kumar]
+ [].
