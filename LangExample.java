041ceee75467a474b7f0748df45d28638e87aeca
saikalyansennam <saikalyansennam66@gmail.com>
2025-03-12 12:33:14 +0530

    explorelangExample

---
+++ LangExample.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LangExample{
+	public String toString()
+	{
+		return"test";
+	}
+	public static void main(String[] args){
+		String h = new String("pawan");
+		System.out.println(h);
+		Integer i = new Integer(30);
+		System.out.println(i);
+		ArrayList f = new ArrayList();
+		f.add("k");
+		f.add("j");
+		f.add("l");
+		System.out.println(f);
+		Test t = new Test();
+		System.out.println(t);
+		
+		
+		
+	}
+}
